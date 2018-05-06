6a96db6bc4133e8e6293daa588e40560ee33ce4c
GNEHS <jayuiop@gmail.com>
2018-05-06 20:39:28 +0800

    移除 JQ

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-$(document).ready(function() {
-    $('#welcome .header').addClass('show')
-})
+window.onload = () => {
+
+    document.querySelectorAll("#welcome .header").forEach(function(node) {
+        addClass(node, "inverted")
+    });
+};
+
+function addClass(obj, cls) {
+    var obj_class = obj.className
+    var blank = (obj_class != '') ? ' ' : '';
+    var added = obj_class + blank + cls;
+    obj.className = added;
+}
+
+function removeClass(obj, cls) {
+    var obj_class = ' ' + obj.className + ' ';
+    obj_class = obj_class.replace(/(\s+)/gi, ' ')
+    var removed = obj_class.replace(' ' + cls + ' ', ' ');
+    removed = removed.replace(/(^\s+)|(\s+$)/g, '');
+    obj.className = removed;
+}
